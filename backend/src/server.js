5ad485d3739904983480525039316bf4623c3fb9
michael mcnair <michaeltsuyoshi@gmail.com>
2023-05-05 02:34:41 +0900

    allow cors

--- backend/src/server.js
+++ backend/src/server.js
@@ -13,6 +13,16 @@ import errorHandler from './middleware/error-handler.js';
 import notFound from './middleware/not-found.js';
 import auth from './middleware/authentication.js';
 
+// cors
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  next();
+});
+
 app.use(express.json());
 
 // routes
