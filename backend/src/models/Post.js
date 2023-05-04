c73e9038779caa132edcb7b7940aa1dc5ef30490
michael mcnair <michaeltsuyoshi@gmail.com>
2023-05-05 02:00:19 +0900

    add timestamps

--- backend/src/models/Post.js
+++ backend/src/models/Post.js
@@ -1,16 +1,19 @@
 import mongoose from 'mongoose';
 
-const PostSchema = new mongoose.Schema({
-  textContent: {
-    type: String,
-    maxLength: [50, 'Message must be 50 characters or shorter'],
-    required: [true, 'Please provide text content'],
+const PostSchema = new mongoose.Schema(
+  {
+    textContent: {
+      type: String,
+      maxLength: [50, 'Message must be 50 characters or shorter'],
+      required: [true, 'Please provide text content'],
+    },
+    createdBy: {
+      type: mongoose.Types.ObjectId,
+      ref: 'User',
+      required: [true, 'Please provide user'],
+    },
   },
-  createdBy: {
-    type: mongoose.Types.ObjectId,
-    ref: 'User',
-    required: [true, 'Please provide user'],
-  },
-});
+  { timestamps: true }
+);
 
 export default mongoose.model('posts', PostSchema);
