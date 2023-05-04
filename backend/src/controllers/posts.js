776b0fae19d8842ffc5f4fabd23d63fb1592bc22
michael mcnair <michaeltsuyoshi@gmail.com>
2023-05-05 01:59:41 +0900

    implement posts controllers

--- backend/src/controllers/posts.js
+++ backend/src/controllers/posts.js
@@ -1,56 +1,74 @@
 import Post from '../models/Post.js';
+import { NotFoundError, BadRequestError } from '../errors/index.js';
+import asyncWrapper from '../middleware/async-wrapper.js';
 
 const getAllPosts = async (req, res) => {
-  try {
-    const posts = await Post.find({});
-    res.status(200).json({ success: true, posts });
-  } catch (err) {
-    console.log(err);
-  }
+  const posts = await Post.find({});
+  res.status(200).json({ success: true, posts });
 };
 
-const getSinglePost = async (req, res) => {
-  try {
-    const { id: postID } = req.params;
-    const post = await Post.findOne({ _id: postID });
-    if (!post) {
-      return res
-        .status(404)
-        .json({ success: false, msg: `id: ${postID} not found` });
+const getSinglePost = asyncWrapper(async (req, res) => {
+  const { id: postId } = req.params;
+
+  const post = await Post.findOne({ _id: postId });
+
+  if (!post) {
+    throw new NotFoundError(`No post with id ${postId}`);
+  }
+
+  res.status(200).json({ success: true, post });
+});
+
+const createPost = asyncWrapper(async (req, res) => {
+  req.body.createdBy = req.user.userId;
+
+  const post = await Post.create(req.body);
+
+  res.status(201).json({ success: true, post });
+});
+
+const deletePost = asyncWrapper(async (req, res) => {
+  const {
+    user: { userId },
+    params: { id: postId },
+  } = req;
+
+  const post = await Post.findOneAndDelete({ _id: postId, createdBy: userId });
+
+  if (!post) {
+    throw new NotFoundError(`No post with id ${postId}`);
+  }
+
+  res.status(200).json({ success: true, id: post.id });
+});
+
+const updatePost = asyncWrapper(async (req, res) => {
+  const {
+    body: { textContent },
+    user: { userId },
+    params: { id: postId },
+  } = req;
+
+  if (textContent === '') {
+    throw new BadRequestError('Text content can not be empty');
+  }
+
+  const post = await Post.findOneAndUpdate(
+    { _id: postId, createdBy: userId },
+    { textContent: textContent },
+    {
+      new: true,
+      runValidators: true,
     }
+  );
 
-    res.status(200).json({ success: true, post });
-  } catch (err) {
-    console.log(err);
+  if (!post) {
+    throw new NotFoundError(`No post with id ${postId}`);
   }
-};
 
-const createPost = async (req, res) => {
-  try {
-    const post = await Post.create(req.body);
-    res.status(201).json({ success: true, post });
-  } catch (err) {
-    console.log(err);
-  }
-};
-
-const deletePost = async (req, res) => {
-  try {
-    const { id: postID } = req.params;
-    const post = await Post.findOneAndDelete({ _id: postID });
-    if (!post) {
-      return res
-        .status(404)
-        .json({ success: false, msg: `id: ${postID} not found` });
-    }
-    res.status(200).json({ post });
-  } catch (err) {
-    console.log(err);
-  }
-};
-
-const updatePost = async (req, res) => {
-  res.send('patch');
-};
+  res
+    .status(200)
+    .json({ success: true, post: { textContent: post.textContent } });
+});
 
 export { getAllPosts, getSinglePost, createPost, deletePost, updatePost };
