3450af871a16e2c2cfbf249c08feb120c00b64a2
michael mcnair <michaeltsuyoshi@gmail.com>
2023-05-05 02:00:05 +0900

    implement error handler

--- backend/src/middleware/error-handler.js
+++ backend/src/middleware/error-handler.js
@@ -1,6 +1,28 @@
-const errorHandler = (err, req, res, next) => {
-  console.log(err);
-  next();
-};
+export default function errorHandler(err, req, res, next) {
+  console.log('error handler called');
+  let customError = {
+    statusCode: err.statusCode || 500,
+    msg: err.message || 'Something went wrong try again later',
+  };
 
-export default errorHandler;
+  if (err.name === 'ValidationError') {
+    customError.msg = Object.values(err.errors)
+      .map((item) => item.message)
+      .join(`,\xa0`);
+    customError.statusCode = 400;
+  }
+
+  if (err.code && err.code === 11000) {
+    customError.msg = `Duplicate value entered for ${
+      Object.keys(err.keyValue)[0]
+    } field, please choose another value.`;
+    customError.statusCode = 400;
+  }
+
+  if (err.name === 'CastError') {
+    customError.msg = `No item found with id: ${err.value}`;
+    customError.statusCode = 404;
+  }
+
+  return res.status(customError.statusCode).json({ msg: customError.msg });
+}
