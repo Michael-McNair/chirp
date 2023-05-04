1d73b687489d8f6a33a4a84d2084286f8935f3c9
michael mcnair <michaeltsuyoshi@gmail.com>
2023-05-05 01:59:31 +0900

    implement auth controllers

--- backend/src/controllers/auth.js
+++ backend/src/controllers/auth.js
@@ -1,32 +1,28 @@
-import jwt from 'jsonwebtoken';
 import User from '../models/User.js';
+import { BadRequestError, UnauthenticatedError } from '../errors/index.js';
 
 const register = async (req, res) => {
-  try {
-    const user = await User.create(req.body);
-    const token = user.createJWT();
+  const user = await User.create(req.body);
+  const token = user.createJWT();
 
-    res.status(201).json({ name: user.name, token: token });
-  } catch (err) {
-    res.status(400).json(err);
-  }
+  res.status(201).json({ name: user.name, token: token });
 };
 
 const login = async (req, res) => {
   const { email, password } = req.body;
 
   if (!email || !password) {
-    res.status(400).send('Please provide email and password');
+    throw new BadRequestError('Please provide email and password');
   }
 
   const user = await User.findOne({ email });
   if (!user) {
-    res.status(400).send('Invalid Credentials');
+    throw new UnauthenticatedError('Invalid Credentials');
   }
 
   const isPasswordCorrect = await user.comparePassword(password);
   if (!isPasswordCorrect) {
-    res.status(400).send('Invalid Credentials');
+    throw new UnauthenticatedError('Invalid Credentials');
   }
 
   const token = user.createJWT();
