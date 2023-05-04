0a4e6be61ca6f1efa22810c21a5a07fc0d1bf20a
michael mcnair <michaeltsuyoshi@gmail.com>
2023-05-04 22:22:29 +0900

    add createdBy to post schema

--- backend/src/models/Post.js
+++ backend/src/models/Post.js
@@ -6,6 +6,11 @@ const PostSchema = new mongoose.Schema({
     maxLength: [50, 'Message must be 50 characters or shorter'],
     required: [true, 'Please provide text content'],
   },
+  createdBy: {
+    type: mongoose.Types.ObjectId,
+    ref: 'User',
+    required: [true, 'Please provide user'],
+  },
 });
 
 export default mongoose.model('posts', PostSchema);
