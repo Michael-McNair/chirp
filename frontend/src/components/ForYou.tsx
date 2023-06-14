81a47d87de3cb52ec56486d7f3393357f5f0bca9
Michael McNair <michaeltsuyoshi@gmail.com>
2023-06-14 10:07:22 +0900

    implement icon and name with link

--- frontend/src/components/ForYou.tsx
+++ frontend/src/components/ForYou.tsx
@@ -1,6 +1,9 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 
+import Icon from './Icon';
+import Name from './Name';
+
 export default function ForYou() {
   const [results, setResults] = useState([]);
 
@@ -24,15 +27,23 @@ export default function ForYou() {
       {results.map(
         (result: {
           _id: string;
-          createdBy: { name: string };
+          createdBy: { name: string; color: string; _id: string };
           textContent: string;
         }) => {
+          console.log(result.createdBy);
           return (
             <div
               key={result._id}
               className="border-black border-solid border-2 my-1"
             >
-              <p>{result.createdBy.name}</p>
+              <div className="flex items-center gap-1 justify-start">
+                <Icon
+                  size={8}
+                  color={result.createdBy.color}
+                  userName={result.createdBy.name}
+                />
+                <Name name={result.createdBy.name} _id={result.createdBy._id} />
+              </div>
               <h2>{result.textContent}</h2>
             </div>
           );
