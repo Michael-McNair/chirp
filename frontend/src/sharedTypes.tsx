172f46bdbb08b762e297d18e16d578dad08f49eb
Michael McNair <michaeltsuyoshi@gmail.com>
2023-07-05 09:14:17 +0900

    add post type

--- frontend/src/sharedTypes.tsx
+++ frontend/src/sharedTypes.tsx
@@ -3,4 +3,13 @@ export interface User {
   name: string;
   email: string;
   color: string;
+  following: [];
+}
+
+export interface Post {
+  _id: string;
+  textContent: string;
+  createdBy: { _id: string; name: string };
+  createdAt: string;
+  updatedAt: string;
 }
