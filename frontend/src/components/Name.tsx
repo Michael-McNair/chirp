38f5755d16908dd6cef8257008e456ab65a25c7a
Michael McNair <michaeltsuyoshi@gmail.com>
2023-07-05 09:15:12 +0900

    allow custom css

--- frontend/src/components/Name.tsx
+++ frontend/src/components/Name.tsx
@@ -1,9 +1,13 @@
 import { Link } from 'react-router-dom';
 
-export default function Name(props: { name: string; _id: string }) {
+export default function Name(props: {
+  name: string;
+  _id: string;
+  className: string;
+}) {
   return (
     <Link to={`/user/${props._id}`}>
-      <h4>{props.name}</h4>
+      <h4 className={props.className}>{props.name}</h4>
     </Link>
   );
 }
