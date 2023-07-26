7303647e16a00bdf993fa18144a67ab7aeed3e98
Michael McNair <michaeltsuyoshi@gmail.com>
2023-07-26 15:08:27 +0900

    fix for you/following page indicator

--- frontend/src/routes/Home.tsx
+++ frontend/src/routes/Home.tsx
@@ -1,10 +1,20 @@
 import Nav from '../components/Nav';
+import WhoToFollow from '../components/WhoToFollow.tsx';
 
 import { User } from '../sharedTypes.tsx';
 
-import { Outlet, Link } from 'react-router-dom';
+import { Outlet, Link, useLocation } from 'react-router-dom';
+import { useState, useEffect } from 'react';
 
 export default function Home(props: { user: User }) {
+  const location = useLocation();
+
+  const [page, setPage] = useState<string>();
+
+  useEffect(() => {
+    setPage(location.pathname.split('/')[2]);
+  }, [location]);
+
   return (
     <div className="w-full flex gap-6">
       <Nav user={props.user} />
@@ -17,9 +27,9 @@ export default function Home(props: { user: User }) {
             >
               For you
             </Link>
-            {/* {page === 'for-you' && (
-            <div className="absolute bottom-0 right-1/2 translate-x-1/2 w-3 h-1 bg-blue-600"></div>
-          )} */}
+            {page === 'for-you' && (
+              <div className="absolute bottom-0 right-1/2 translate-x-1/2 w-3 h-1 bg-blue-600"></div>
+            )}
           </div>
           <div className="h-12 flex-1 flex justify-center relative">
             <Link
@@ -28,19 +38,23 @@ export default function Home(props: { user: User }) {
             >
               Following
             </Link>
-            {/* {page === 'following' && (
-            <div className="absolute bottom-0 right-1/2 translate-x-1/2 w-3 h-1 bg-blue-600"></div>
-          )} */}
+            {page === 'following' && (
+              <div className="absolute bottom-0 right-1/2 translate-x-1/2 w-3 h-1 bg-blue-600"></div>
+            )}
           </div>
         </div>
 
         <Outlet />
       </div>
-      <div className="w-40">
-        <Link to="/login">login</Link>
-        <br />
-        <Link to="/register">register</Link>
-      </div>
+      {props.user ? (
+        <WhoToFollow />
+      ) : (
+        <div className="w-40">
+          <Link to="/login">login</Link>
+          <br />
+          <Link to="/register">register</Link>
+        </div>
+      )}
     </div>
   );
 }
