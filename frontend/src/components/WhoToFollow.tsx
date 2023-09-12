eb4213077f57a481e4cd03d9611d1a0047b35c2e
Michael McNair <michaeltsuyoshi@gmail.com>
2023-09-12 13:51:46 +0900

    implement who to follow menu

--- frontend/src/components/WhoToFollow.tsx
+++ frontend/src/components/WhoToFollow.tsx
@@ -1,10 +1,37 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+
+import Icon from './Icon';
+
+import { User } from '../sharedTypes';
+
 export default function WhoToFollow() {
+  const [result, setResult] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3000/api/v1/users-to-follow')
+      .then((res) => setResult(res.data.response));
+  }, []);
+
   return (
-    <section className="h-screen w-44 flex-shrink-0">
+    <section className="h-screen w-48 flex-shrink-0">
       <div className="w-full h-0"></div>
 
-      <div className="flex flex-col justify-stretch fixed w-44 h-[calc(100%-4rem)] my-8 p-5 rounded-md bg-slate-50 shadow-md">
-        <h2 className="text-xl">who to follow</h2>
+      <div className="flex flex-col justify-stretch fixed w-48 h-[calc(100%-4rem)] my-8 rounded-md bg-slate-50 shadow-md">
+        <h2 className="text-2xl mb-3 px-5 pt-5">who to follow</h2>
+
+        {result.map((user: User) => (
+          <Link
+            to={`/home/user/${user.id}`}
+            key={user.id}
+            className="flex gap-1.5 items-center py-2 px-3 hover:bg-slate-200"
+          >
+            <Icon size={7} color={user.color} name={user.name} />
+            <h4 className="text-lg">{user.name}</h4>
+          </Link>
+        ))}
       </div>
     </section>
   );
