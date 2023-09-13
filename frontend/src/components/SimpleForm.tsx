72d8f6d22c4b2555290974abb39518f7e502cb3d
Michael McNair <michaeltsuyoshi@gmail.com>
2023-09-13 14:34:56 +0900

    change simple form for more flexibility

--- frontend/src/components/SimpleForm.tsx
+++ frontend/src/components/SimpleForm.tsx
@@ -26,43 +26,40 @@ export default function SimpleForm(props: {
   const [formStates, setFormStates] = useState(defaultStates);
 
   return (
-    <div className="h-screen w-full flex items-center justify-center relative">
-      <form
-        className="flex gap-2 flex-col rounded-md shadow-md w-96 w-2/1 p-6 pt-5 bg-slate-50"
-        onSubmit={(e) => {
-          e.preventDefault();
-          props.onSubmit(formStates);
-        }}
+    <form
+      className="flex gap-2 flex-col rounded-md shadow-md w-96 w-2/1 p-6 pt-5 bg-slate-50"
+      onSubmit={(e) => {
+        e.preventDefault();
+        props.onSubmit(formStates);
+      }}
+    >
+      {props.inputs.map((input, i) => {
+        return (
+          <label key={i}>
+            {input.label}
+            <input
+              className="w-full p-2 focus:outline-none bg-slate-200"
+              placeholder={input.placeholder}
+              type={input.type}
+              value={formStates[input.variable]}
+              name={input.variable}
+              onChange={(e: any) => {
+                let newFormStates = Object.assign({}, formStates);
+
+                newFormStates[e.target.attributes.name.value] = e.target.value;
+
+                setFormStates(newFormStates);
+              }}
+            />
+          </label>
+        );
+      })}
+      <button
+        className="px-4 py-2 mt-3 bg-black text-white text-xl"
+        type="submit"
       >
-        {props.inputs.map((input, i) => {
-          return (
-            <label key={i}>
-              {input.label}
-              <input
-                className="w-full p-2 focus:outline-none bg-slate-200"
-                placeholder={input.placeholder}
-                type={input.type}
-                value={formStates[input.variable]}
-                name={input.variable}
-                onChange={(e: any) => {
-                  let newFormStates = Object.assign({}, formStates);
-
-                  newFormStates[e.target.attributes.name.value] =
-                    e.target.value;
-
-                  setFormStates(newFormStates);
-                }}
-              />
-            </label>
-          );
-        })}
-        <button
-          className="px-4 py-2 mt-3 bg-black text-white text-xl"
-          type="submit"
-        >
-          {props.button}
-        </button>
-      </form>
-    </div>
+        {props.button}
+      </button>
+    </form>
   );
 }
