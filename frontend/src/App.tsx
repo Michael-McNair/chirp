aa47accf208b8c8816345394b31eb20a42077f62
Michael McNair <michaeltsuyoshi@gmail.com>
2023-09-13 14:19:17 +0900

    add 404 page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,10 @@
+import { Route, Routes, Navigate } from 'react-router-dom';
+
 import Home from './routes/Home.tsx';
 import Register from './routes/Register.tsx';
 import Login from './routes/Login.tsx';
 import Post from './routes/MakePost.tsx';
-import ForYou from './routes/ForYou.tsx';
-import Following from './routes/Following.tsx';
-import UserPage from './routes/UserPage.tsx';
-
-import { User } from './sharedTypes.tsx';
-
-import { Route, Routes } from 'react-router-dom';
-import { useState, useEffect } from 'react';
-import axios from 'axios';
+import NotFound from './routes/NotFound.tsx';
 
 export default function App() {
   return (
@@ -20,7 +14,8 @@ export default function App() {
         <Route path="/register" element={<Register />} />
         <Route path="/post" element={<Post />} />
         <Route path="/home/*" element={<Home />} />
-        <Route path="*" element={<h1>no match</h1>} />
+        <Route path="/" element={<Navigate to="/home" />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </div>
   );
---
+++ frontend/src/routes/NotFound.tsx
@@ -0,0 +1,13 @@
+import { Link } from 'react-router-dom';
+
+export default function NotFound() {
+  return (
+    <div className="w-full h-screen flex flex-col items-center justify-center">
+      <h1 className="text-9xl font-extrabold">404</h1>
+      <p className="text-xl my-3">Page not found</p>
+      <Link to="/" className="text-blue-700 text-2xl hover:underline">
+        Go To Webapp
+      </Link>
+    </div>
+  );
+}
