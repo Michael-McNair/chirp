f70bf213857794ee711f1a39d0a4cd0d2caa93b7
Michael McNair <michaeltsuyoshi@gmail.com>
2023-10-04 13:01:46 +0900

    implement time to posts

--- frontend/src/components/PostComponent.tsx
+++ frontend/src/components/PostComponent.tsx
@@ -6,19 +6,51 @@ import { Post } from '../sharedTypes.tsx';
 export default function PostComponent(props: { post: Post }) {
   const { post } = props;
 
+  function formatTimeAgo(timestamp: string) {
+    const now = new Date();
+    const createdAt = new Date(timestamp);
+
+    const timeDifference = now.getTime() - createdAt.getTime();
+    const seconds = Math.floor(timeDifference / 1000);
+    const minutes = Math.floor(seconds / 60);
+    const hours = Math.floor(minutes / 60);
+    const days = Math.floor(hours / 24);
+    const months = Math.floor(days / 30.44); // Approximate number of days in a month
+    const years = Math.floor(months / 12);
+
+    if (years > 0) {
+      return `${years}y`;
+    } else if (months > 0) {
+      return `${months}mo`;
+    } else if (days > 0) {
+      return `${days}d`;
+    } else if (hours > 0) {
+      return `${hours}h`;
+    } else if (minutes > 0) {
+      return `${minutes}m`;
+    } else {
+      return `${seconds}s`;
+    }
+  }
+
   return (
     <div className="mb-4 sm:mb-6">
-      <div className="flex items-center gap-3 justify-start">
-        <Icon
-          size={12}
-          color={post.createdBy.color}
-          name={post.createdBy.name}
-        />
-        <Name
-          name={post.createdBy.name}
-          _id={post.createdBy._id}
-          className={'text-xl sm:text-2xl'}
-        />
+      <div className="flex items-center justify-between">
+        <div className="flex items-center gap-3 justify-start">
+          <Icon
+            size={12}
+            color={post.createdBy.color}
+            name={post.createdBy.name}
+          />
+          <Name
+            name={post.createdBy.name}
+            _id={post.createdBy._id}
+            className={'text-xl sm:text-2xl'}
+          />
+        </div>
+        <h3 className="text-md sm:text-lg text-slate-500">
+          {formatTimeAgo(post.createdAt)}
+        </h3>
       </div>
       <h2 className="text-lg sm:text-xl mt-2 sm:mt-3">{post.textContent}</h2>
     </div>
