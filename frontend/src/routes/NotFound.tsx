2a27b6905bf453879cbb6d6da66f1649719fe6e4
Michael McNair <michaeltsuyoshi@gmail.com>
2023-10-11 12:09:42 +0900

    improve 404 page

--- frontend/src/routes/NotFound.tsx
+++ frontend/src/routes/NotFound.tsx
@@ -6,7 +6,7 @@ export default function NotFound() {
       <h1 className="text-9xl font-extrabold">404</h1>
       <p className="text-xl my-3">Page not found</p>
       <Link to="/" className="text-blue-700 text-2xl hover:underline">
-        Go To Webapp
+        Go To Home
       </Link>
     </div>
   );
