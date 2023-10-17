8444bce83128955e2ff53321ad578f24d4583e1f
Michael McNair <michaeltsuyoshi@gmail.com>
2023-10-17 14:41:21 +0900

    implement popup component

---
+++ frontend/src/components/Popup.tsx
@@ -0,0 +1,32 @@
+export default function Popup(props: {
+  textContent: string;
+  shown: boolean;
+  setPopupShown: any;
+}) {
+  return (
+    <div
+      className={`${
+        props.shown ? 'block' : 'hidden'
+      } fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-slate-50 shadow-md rounded-md p-10`}
+    >
+      <button
+        className="w-10 h-10 relative mb-3"
+        onClick={() => props.setPopupShown(false)}
+      >
+        <div className="h-1 w-full bg-black absolute top-1/2 -translate-y-1/2 rotate-45"></div>
+        <div className="h-1 w-full bg-black absolute top-1/2 -translate-y-1/2 -rotate-45"></div>
+      </button>
+
+      <h2 className="text-center text-4xl mb-10 whitespace-nowrap">
+        {props.textContent}
+      </h2>
+
+      <button
+        className="bg-black text-white p-3 text-3xl w-full"
+        onClick={() => props.setPopupShown(false)}
+      >
+        Close
+      </button>
+    </div>
+  );
+}
